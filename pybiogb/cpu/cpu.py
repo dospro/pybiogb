f265ae33d596855e3981f4be008609a8ee578aa7
dospro <dospro@gmail.com>
2018-03-07 13:18:05 -0600

    Fully implemented a set of ld operations. * Used new technique gor generating functions

--- pybiogb/cpu/cpu.py
+++ pybiogb/cpu/cpu.py
@@ -10,6 +10,85 @@ class Cpu:
         self.registers = Registers()
         self.memory = Memory()
         self.opcodes = {
+            # LD nn, n
+            0x06: self.ld_n('b_register'),
+            0x0E: self.ld_n('c_register'),
+            0x16: self.ld_n('d_register'),
+            0x1E: self.ld_n('e_register'),
+            0x26: self.ld_n('h_register'),
+            0x2E: self.ld_n('l_register'),
+
+            # LD r1, r2
+            0x7F: self.ld('a_register', 'a_register'),
+            0x78: self.ld('a_register', 'b_register'),
+            0x79: self.ld('a_register', 'c_register'),
+            0x7A: self.ld('a_register', 'd_register'),
+            0x7B: self.ld('a_register', 'e_register'),
+            0x7C: self.ld('a_register', 'h_register'),
+            0x7D: self.ld('a_register', 'l_register'),
+            0x7E: self.ld_r_hl('a_register'),
+
+            0x40: self.ld('b_register', 'b_register'),
+            0x41: self.ld('b_register', 'c_register'),
+            0x42: self.ld('b_register', 'd_register'),
+            0x43: self.ld('b_register', 'e_register'),
+            0x44: self.ld('b_register', 'h_register'),
+            0x45: self.ld('b_register', 'l_register'),
+            0x46: self.ld_r_hl('b_register'),
+
+            0x48: self.ld('c_register', 'b_register'),
+            0x49: self.ld('c_register', 'c_register'),
+            0x4A: self.ld('c_register', 'd_register'),
+            0x4B: self.ld('c_register', 'e_register'),
+            0x4C: self.ld('c_register', 'h_register'),
+            0x4D: self.ld('c_register', 'l_register'),
+            0x4E: self.ld_r_hl('c_register'),
+
+            0x50: self.ld('d_register', 'b_register'),
+            0x51: self.ld('d_register', 'c_register'),
+            0x52: self.ld('d_register', 'd_register'),
+            0x53: self.ld('d_register', 'e_register'),
+            0x54: self.ld('d_register', 'h_register'),
+            0x55: self.ld('d_register', 'l_register'),
+            0x56: self.ld_r_hl('d_register'),
+
+            0x58: self.ld('e_register', 'b_register'),
+            0x59: self.ld('e_register', 'c_register'),
+            0x5A: self.ld('e_register', 'd_register'),
+            0x5B: self.ld('e_register', 'e_register'),
+            0x5C: self.ld('e_register', 'h_register'),
+            0x5D: self.ld('e_register', 'l_register'),
+            0x5E: self.ld_r_hl('e_register'),
+
+            0x60: self.ld('h_register', 'b_register'),
+            0x61: self.ld('h_register', 'c_register'),
+            0x62: self.ld('h_register', 'd_register'),
+            0x63: self.ld('h_register', 'e_register'),
+            0x64: self.ld('h_register', 'h_register'),
+            0x65: self.ld('h_register', 'l_register'),
+            0x66: self.ld_r_hl('h_register'),
+
+            0x68: self.ld('l_register', 'b_register'),
+            0x69: self.ld('l_register', 'c_register'),
+            0x6A: self.ld('l_register', 'd_register'),
+            0x6B: self.ld('l_register', 'e_register'),
+            0x6C: self.ld('l_register', 'h_register'),
+            0x6D: self.ld('l_register', 'l_register'),
+            0x6E: self.ld_r_hl('l_register'),
+
+            0x70: self.ld_hl_r('b_register'),
+            0x71: self.ld_hl_r('c_register'),
+            0x72: self.ld_hl_r('d_register'),
+            0x73: self.ld_hl_r('e_register'),
+            0x74: self.ld_hl_r('h_register'),
+            0x75: self.ld_hl_r('l_register'),
+            0x36: self.ld_hl_n,
+
+            0x0A: self.ld_a_rr('bc_register'),
+            0x1A: self.ld_a_rr('de_register'),
+            0xFA: self.ld_a_nn,
+            0x3E: self.ld_n('a_register'),
+
             0x87: partial(self.add, self.registers.a_register),
             0x80: partial(self.add, self.registers.b_register),
             0x81: partial(self.add, self.registers.c_register),
@@ -192,8 +271,54 @@ class Cpu:
         self.registers.sp_register -= 1
         self.memory.write_byte(self.registers.sp_register, register_value & 0xFF)
 
-    def ld(self, src_name, dst_name):
-        setattr(self.registers, dst_name, getattr(self.registers, src_name))
+    def ld(self, dst_register, src_register):
+        registers = vars(self.registers)
+
+        def real_ld():
+            registers[dst_register] = registers[src_register]
+
+        return real_ld
+
+    def ld_n(self, dst_register):
+        registers = vars(self.registers)
+
+        def real_ld_n():
+            registers[dst_register] = self.read_next_byte()
+
+        return real_ld_n
+
+    def ld_r_hl(self, dst_register):
+        registers = vars(self.registers)
+
+        def real_r_ld():
+            registers[dst_register] = self.memory.read_byte(self.registers.hl_register)
+
+        return real_r_ld
+
+    def ld_hl_r(self, src_register):
+        registers = vars(self.registers)
+
+        def real_ld_hl_r():
+            self.memory.write_byte(self.registers.hl_register, registers[src_register])
+
+        return real_ld_hl_r
+
+    def ld_hl_n(self):
+        self.memory.write_byte(self.registers.hl_register, self.read_next_byte())
+
+    def ld_a_rr(self, src_register):
+        registers = vars(self.registers)
+
+        def real_a_rr():
+            self.registers.a_register = self.memory.read_byte(registers[src_register])
+
+        return real_a_rr
+
+    def ld_a_nn(self):
+        low_byte = self.read_next_byte()
+        hi_value = self.read_next_byte()
+        address = ((hi_value << 8) | low_byte) & 0xFFFF
+        self.registers.a_register = self.memory.read_byte(address)
 
     def sub(self, value):
         self.registers.z_flag = self.registers.a_register == value
